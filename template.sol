740836e024406348bfaba97f8940f0d9cc358fe6
Sakshya2308 <87920148+Sakshya2308@users.noreply.github.com>
2023-01-12 10:23:04 +0530

    Create template.sol

---
+++ template.sol
@@ -0,0 +1,16 @@
+//Normal setup of our scripts
+const ethers = require("ethers");
+
+
+async function main(){
+
+//http://127.0.0.1:7545
+}
+
+// don't focus on this for now.
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    });
